348393c3ab4adeaca53fd2d6b984c7ba69050146
artur85lk <artur85lk@o2.pl>
2022-09-08 02:18:03 -0700

    2/4

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -10,4 +10,10 @@ else:
         firstname = input()
         surname = "0"
         names[firstname] = 0
+    print("Enter the total bill value:")
+    bill = input()
+    bill_one_person = round(float(bill) / number_of_friends, 2)
+    for i,j in names.items():
+        names[i] = bill_one_person
     print(names)
+
