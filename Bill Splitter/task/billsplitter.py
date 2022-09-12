622212febef0643028888a811fad4c7c660ba0b5
artur85lk <artur85lk@o2.pl>
2022-09-12 02:58:47 -0700

    # 4/4 9.09.2022

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -2,11 +2,18 @@ import random
 
 print("Enter the number of friends joining (including you):")
 number_of_friends = int(input())
+print()
 names = {}
-def lucky_man(choice):
+def lucky_man(choice, bill):
     if choice == "Yes":
         lucky_name = random.choice(list(names.keys()))
         print(f"{lucky_name} is the lucky one!")
+        newbill = round(float(bill) / (number_of_friends - 1), 2)
+        for i,j in names.items():
+            if i == lucky_name:
+                names[lucky_name] = 0
+            else:
+                names[i] = newbill
     else:
         print("No one is going to be lucky")
 
@@ -18,13 +25,18 @@ else:
         firstname = input()
         surname = "0"
         names[firstname] = 0
+    print()
     print("Enter the total bill value:")
     bill = input()
+    print()
     bill_one_person = round(float(bill) / number_of_friends, 2)
     for i,j in names.items():
         names[i] = bill_one_person
     # print(names)
     print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
     choice = input()
-    lucky_man(choice)
-
+    print()
+    lucky_man(choice,bill)
+    print()
+if number_of_friends > 0:
+    print(names)
